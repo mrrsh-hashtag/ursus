3d4cee794b7ab795423ab136271f74a0f4529708
Olov Nykvist <olov@fieldstream.com>
2022-01-04 19:26:38 +0100

    Environment var config

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+log_file = os.environ.get("URSUS_LOG_FILE")
+worker_host = os.environ.get("URSUS_WORKER_HOST")
+
+mongo_url = os.environ.get("URSUS_MONGO_URL")
+mongo_usr = os.environ.get("URSUS_MONGO_USER")
+mongo_psw = os.environ.get("URSUS_MONGO_PSW")
+
+mongo_url = mongo_url.replace("<username>", mongo_usr).replace("<password>", mongo_psw)
