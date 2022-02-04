ed0278798df96c60fc3eee2ddf1b7a74d1de71d1
Olov Nykvist <olov@fieldstream.com>
2022-02-04 16:40:01 +0100

    check and warn env vars

--- config.py
+++ config.py
@@ -8,4 +8,8 @@ mongo_url = os.environ.get("URSUS_MONGO_URL")
 mongo_usr = os.environ.get("URSUS_MONGO_USER")
 mongo_psw = os.environ.get("URSUS_MONGO_PSW")
 
+
+for variable in [log_file, worker_host, worker_url, mongo_url, mongo_usr, mongo_psw]:
+    if variable is None:
+        print(f"Warning: Environment variable {variable} not found")
 mongo_url = mongo_url.replace("<username>", mongo_usr).replace("<password>", mongo_psw)
