cd0ab28ff5881ccf44f943035c5f68f1f38035d1
Olov Nykvist <olov@fieldstream.com>
2022-02-24 12:09:33 +0100

    new test cases

--- test.py
+++ test.py
@@ -20,8 +20,110 @@ params = {
         "facebook_I",
         "competitor_sales_B",
     ],
-    "budget": 2000,
-    # "auto_weight_season": True,
+    "thetas": {
+        "tv_S": {
+            "lower": 0.4,
+            "upper": 0.99
+        },
+        "ooh_S": {
+            "lower": 0.4,
+            "upper": 0.99
+        },
+        "search_S": {
+            "lower": 0.0,
+            "upper": 0.5
+        },
+        "facebook_S": {
+            "lower": 0.0,
+            "upper": 0.5
+        },
+    },
+    "taus": {
+        "tv_S": {
+            "lower": 4,
+        },
+        "ooh_S": {
+            "lower": 4,
+        },
+        "search_S": {
+            "lower": 0.5,
+            "upper": 2
+        },
+        "facebook_S": {
+            "lower": 0.5,
+            "upper": 2
+        },
+    },
+    "budget": 5000,
+    "trials": 1,
+    # "budget": 500,
+    "auto_weight_season": True,
+    "print_progress": True,
+    "plot_one_page": True,
+    "adstock_algo": "weibull",
+    # "adstock_algo": "geometric",
+    # "country_code": "DE"
+}
+
+params_KK = {
+    "customer": "",
+    "frame_name": "",
+    "dep_var": "Conversions",
+    "date_var": "Date",
+    "ind_var": [
+        "TV",
+        "OLV",
+        "OOH",
+        "Radio",
+        "Display",
+        "Mail",
+        "Influencers",
+        "Print",
+        "PaidSocial",
+        "Search"
+
+    ],
+    "context": [
+        "Elpris",
+        "BankID"
+    ],
+    "thetas": {
+        "tv_S": {
+            "lower": 0.4,
+            "upper": 0.99
+        },
+        "ooh_S": {
+            "lower": 0.4,
+            "upper": 0.99
+        },
+        "search_S": {
+            "lower": 0.0,
+            "upper": 0.5
+        },
+        "facebook_S": {
+            "lower": 0.0,
+            "upper": 0.5
+        },
+    },
+    "taus": {
+        "tv_S": {
+            "lower": 4,
+        },
+        "ooh_S": {
+            "lower": 4,
+        },
+        "search_S": {
+            "lower": 0.5,
+            "upper": 2
+        },
+        "facebook_S": {
+            "lower": 0.5,
+            "upper": 2
+        },
+    },
+    "budget": 5000,
+    # "budget": 500,
+    "auto_weight_season": True,
     "print_progress": True,
     "plot_one_page": True,
     # "adstock_algo": "weibull",
@@ -29,10 +131,61 @@ params = {
     # "country_code": "DE"
 }
 
+params_LV = {
+    "customer": "Demo Company",
+    "frame_name": "New Deposit Customers",
+    "dep_var": "NDC",
+    "date_var": "date",
+    "ind_var": [
+        "apple_app_store_EUR",
+        "display_direct_EUR",
+        "display_programmatic_EUR",
+        "facebook_app_EUR",
+        "facebook_web_EUR",
+        "google_play_store_EUR",
+        "google_search_branded_EUR",
+        "google_search_furtive_EUR",
+        "google_search_generic_EUR",
+        "radio_EUR",
+        "snapchat_app_EUR",
+        "tv_EUR",
+        "youtube_EUR"
+    ],
+    "context": [
+        "competition_TRP"
+    ],
+    
+    # "budget": 2000,
+    "budget": 60,
+    # "budget": 500,
+    # "auto_weight_season": True,
+    "print_progress": True,
+    "plot_one_page": True,
+    # "adstock_algo": "weibull",
+    "adstock_algo": "geometric",
+    # "country_code": "DE"
+    "trials": 5
+}
+
+
 ursus = Ursus("demo_data.csv", params)
+# ursus = Ursus("KK_agg.csv", params_KK)
+# ursus = Ursus("LV.csv", params_LV)
+
 # ursus.workshop_test()
 ursus.train()
 # ursus.get_budget_optimization()
+
+while True:
+    print("Low risk")
+    ursus.get_budget_optimization()
+    print("High risk")
+    ursus.get_budget_optimization(0.6, 1.4)
+    do_retry = input("Generate new opti [y/n]:")
+    if do_retry == "y" or do_retry == "Y":
+        ursus.budget_optimizations = []
+    else:
+        break    
 
 training_data = ursus.get_training_data()
 do_push = input("Push training data [y/n]:")
